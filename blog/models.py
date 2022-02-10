100595c3bb921784a30fa7cb44ab7aedf172319e
Guilhemv <guilhem.vadon@gmail.com>
2022-02-10 18:01:54 +0000

    migrations completed zith User fron AbstractUser

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    profile_pic = models.ImageField(blank=True)
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -29,7 +33,7 @@ class Exercise(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(blank=True)
     category = models.ManyToManyField(Category)
-    # duration = models.IntegerField(blank=True, null=True)
+    duration = models.IntegerField(blank=True, null=True)
     number_of_player = models.IntegerField(blank=True, null=True)
     picture = models.ImageField(blank=True, null=True)
     description = models.TextField(blank=True)
