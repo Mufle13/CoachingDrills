1ac2802303c1382d39de2bec437e8e2f9fd0aa0f
Guilhemv <guilhem.vadon@gmail.com>
2022-02-11 19:27:52 +0000

    signup view with custom form

--- CoachingDrills/urls.py
+++ CoachingDrills/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('compte/nouveau', signup, name='signup')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,31 @@
+from inspect import formatannotation
+import django
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from blog.models import User
 
 # Create your views here.
+
+class CustiomSignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = UserCreationForm.Meta.fields
+
+def signup(request):
+    context= {}
+
+    if request.method == 'POST':
+        form = CustiomSignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Bienvenue !')
+        else:
+            context['errors'] = form.errors
+
+  
+    form = CustiomSignupForm()
+    context['form'] = form
+
+    return render(request, 'signup.html', context=context)
+
