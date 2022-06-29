4ecfcd39e1a07ea7fd599cf0d2ad9d55562adc8d
Mufle13 <guilhem.vadon@gmail.com>
2022-06-29 01:43:27 +0100

    change allowed hosts

--- CoachingDrills/settings.py
+++ CoachingDrills/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ['DEBUG']))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "coachingdrills.herokuapp.com"]
 
 
 # Application definition
